1900c04f324295a3687190f964b7ff7f591e444f
Jan Willem <janwillem.luiten@gmail.com>
2019-02-24 19:32:35 +0100

    Initial commit of sources

---
+++ Csv.test/Tests.fs
@@ -0,0 +1,46 @@
+module Tests
+
+open System
+open Xunit
+open FsUnit
+
+open FParsec
+
+let csvParser = Csv.buildParser ',' '''   // Parser with comma as separator and single quote as quote character
+let tsvParser = Csv.buildParser '\t' '''  // Parser with tab as separator and  single quote as quote character
+
+let runParser parser input =
+  let result = runParserOnString parser () "" input
+  match result with
+      | Success(result, _, _) -> result
+      | Failure(errorMessage, _, _) -> failwith errorMessage
+
+[<Fact>]
+let ``Test csv-parser on single csv line`` () =
+  let actual = runParser csvParser "1,2,3"
+  let expected = [["1";"2";"3"]]
+  actual |> should equal expected
+
+[<Fact>]
+let ``Test csv-parser on single csv line with quoted values`` () =
+  let actual = runParser csvParser "' 1',2,' 3 '"
+  let expected = [[" 1";"2";" 3 "]]
+  actual |> should equal expected
+
+[<Fact>]
+let ``Test csv-parser on single csv line with quoted values using the quote in a field`` () =
+  let actual = runParser csvParser "' 1',2,' 3'' '"
+  let expected = [[" 1";"2";" 3' "]]
+  actual |> should equal expected
+
+[<Fact>]
+let ``Test csv-parser on single csv line with quoted values using newline in a field`` () =
+  let actual = runParser csvParser "' 1',2,' 3\nOn a newline '"
+  let expected = [[" 1";"2";" 3\nOn a newline "]]
+  actual |> should equal expected
+
+[<Fact>]
+let ``Test csv-parser on multiple lines`` () =
+  let actual = runParser csvParser "1,2,3\n4,5,6"
+  let expected = [["1";"2";"3"];["4";"5";"6"]]
+  actual |> should equal expected
---
+++ Csv/Csv.fs
@@ -0,0 +1,44 @@
+module Csv
+
+open FParsec
+
+// csvData ::= (csvRecord)* 'EOF'
+// csvRecord ::= ws* csvFiedlList ('\n' | 'EOF')
+// csvFieldList ::= csvField ws* [sep ws* csvFieldList]
+// csvField ::= rawField | quotedString
+// rawField ::= (any character except '\n' 'EOF' sep)
+// quotedField :== quote [quote quote | char] quote
+// ws :== any character in " \t" except sep
+
+// value restriction (see FParsec documentation)
+type P<'t> = Parser<'t, Unit>
+
+// general helpers
+let debug (p: P<_>) stream =
+    p stream // set a breakpoint here
+
+let first (s: string) = s.Substring(0,1)
+let trim (s: string) = s.Trim()
+
+let doublechar (c: char):P<_> =
+  let ds = [c;c] |> System.String.Concat
+  (pstring ds) |>> first
+
+let quotedstring (quote: char):P<_> =
+  let qstring = string quote
+  let doublequote = doublechar quote
+  let normalCharSnippet = manySatisfy (fun c -> c <> quote)
+  between (pstring qstring) (pstring qstring)
+    (stringsSepBy normalCharSnippet doublequote)
+
+// given a separator character and a quote character, return a parser
+let buildParser (sep: char) (quote: char): P<_> =
+  let ws = if '\t' = sep then " " else " \t"
+  let skipWs = many (skipAnyOf ws)
+  let fieldStopSet = "\n" + string sep
+  let rawString = many (noneOf fieldStopSet) |>> System.String.Concat |>> trim
+  let qstring = (quotedstring quote)
+  let csvField = (qstring <|> rawString) .>> skipWs
+  let csvRecord = skipWs >>. sepBy csvField (skipChar sep .>> skipWs)
+  let csvParser = manyTill (csvRecord .>> (skipNewline <|> eof)) eof
+  csvParser
